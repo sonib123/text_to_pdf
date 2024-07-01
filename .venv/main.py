a92436277bc7d6fecb3bc1a98c2ba78d36531911
sonib <sonibhinder@hotmail.com>
2024-07-01 18:58:19 +0100

    Adding the Header to PDFs

--- .venv/main.py
+++ .venv/main.py
@@ -1,1 +1,15 @@
-import pandas as pd
+import glob
+from fpdf import FPDF
+from pathlib import Path
+
+filepaths = glob.glob("files/*.txt*")
+
+for filepath in filepaths:
+    pdf = FPDF(orientation="P", unit="mm", format="A4")
+    pdf.add_page()
+    filename = Path(filepath).stem
+    filename = filename.title()
+    pdf.set_font(family="Times", size=16, style="B")
+    pdf.cell(w=50, h=8, txt=filename)
+
+    pdf.output(f"PDFs/{filename}.pdf")
